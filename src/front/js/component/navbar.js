d4624a32dc33cbddee809fb978562587c134a462
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-08-19 21:21:47 +0000

    started building landing page

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import logo2 from "../../img/Logo2.png";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light">
+		<nav className="navbar navbar-light bg-white">
 			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
+				<img className="logo" src={logo2}/>
+				<h1 className="diphylleia-regular title">Plant Sitter Pro</h1>
 				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
+					<Link to="#">
+						<button className="btn btn-success">Start Here</button>
 					</Link>
+					{/* <Link to="/demo">
+						<button className="btn btn-primary">Check the Context in action</button>
+					</Link> */}
 				</div>
 			</div>
 		</nav>
