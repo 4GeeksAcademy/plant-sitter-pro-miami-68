c8f01c667aad9c78cd01ef1a2cc125ca3d11a252
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-08-19 23:33:21 +0000

    updated dropdown menu

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -9,9 +9,25 @@ export const Navbar = () => {
 				<img className="logo" src={logo2}/>
 				<h1 className="diphylleia-regular title">Plant Sitter Pro</h1>
 				<div className="ml-auto">
-					<Link to="#">
+					{/* <Link to="#">
 						<button className="btn btn-success">Start Here</button>
-					</Link>
+					</Link> */}
+
+					<div class="dropdown">
+						<button class="btn btn-success dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+							Start Here
+						</button>
+						<ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton1">
+							<li><a class="dropdown-item" href="#">Home</a></li>
+							<li><a class="dropdown-item" href="#">How it Works</a></li>
+							<li><a class="dropdown-item" href="#">Services</a></li>
+							<li><a class="dropdown-item" href="#">For Providers</a></li>
+							<li><a class="dropdown-item" href="#">Blog/Resources</a></li>
+							<li><a class="dropdown-item" href="#">Contact Us</a></li>
+						</ul>
+					</div>
+
+
 					{/* <Link to="/demo">
 						<button className="btn btn-primary">Check the Context in action</button>
 					</Link> */}
