8fecef42a4233f5edca63b629572c287e03cff75
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-08-20 00:27:18 +0000

    updated to include landing page

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -2,7 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
-import houseplants from "../../img/houseplants.jpg";
+import houseplants from "../../img/houseplants2.jpg";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -10,6 +10,7 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1 className="diphylleia-regular">Find Expert Help for Your Plants Anytime, Anywhere</h1>
+			<h5>Whether you're away or need assistance, connect with trusted plant professionals near you.</h5>
 			<p>
 				<img className="homePageImages" src={houseplants} />
 			</p>
@@ -25,3 +26,4 @@ export const Home = () => {
 		</div>
 	);
 };
+
