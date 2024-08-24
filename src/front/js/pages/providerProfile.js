99c409713a0d4d0fa5e13d83319dacb15af5d556
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-08-24 00:12:48 +0000

    make pictures clickable in provider profile file

--- src/front/js/pages/providerProfile.js
+++ src/front/js/pages/providerProfile.js
@@ -32,19 +32,14 @@ export const ProviderProfile = () => {
                     <div data-mdb-input-init className="form-outline form-white">
                         <input type="text" placeholder="First Name" id="form3Examplea5" className="form-control form-control-lg mb-3" />
                         <input type="text" placeholder="Last Name" id="form3Examplea5" className="form-control form-control-lg mb-3" />
-                    {/* </div>
-                    <div data-mdb-input-init className="form-outline form-white"> */}
                         <input type="text" placeholder="Your Location (City, State)" id="form3Examplea5" className="form-control form-control-lg mb-3" />
-                    {/* </div>
-                    <div data-mdb-input-init className="form-outline form-white"> */}
-                        <textarea className="form-control form-control-lg" placeholder="Brief Intro..." aria-label="With textarea">
-                        </textarea>
+                        <textarea className="form-control form-control-lg" placeholder="Brief Intro..." aria-label="With textarea"></textarea>
                     </div>
                 </div>
                 <div className="col bg-success rounded p-3 m-2">
                     <label for="basic-url" className="form-label diphylleia-regular fs-5 text-white"><strong>What should potential clients know about you?</strong></label>
                     <div className="input-group mb-3">
-                        <textarea rows="5" className="form-control" placeholder="I've been a plant love my whole life..." aria-label="With textarea"></textarea>
+                        <textarea rows="5" className="form-control" placeholder="I've been a plant lover my whole life..." aria-label="With textarea"></textarea>
                     </div>
                     <label for="basic-url" className="form-label diphylleia-regular fs-5 text-white"><strong>Do you currently own any plants?</strong></label>
                     <div className="input-group mb-3">
@@ -66,39 +61,55 @@ export const ProviderProfile = () => {
                     <h1 className="diphylleia-regular text-white">What kinds of plants are you comfortable taking care of?</h1>
                     <h5 className="mt-4 mb-4 text-white">Select all that apply</h5>
 
+        {/* code from O'Neil for clickable images */}
+                    {/* <form action="/submit-url" method="post">
+                        <input type="image" src="path/to/your/image.png" alt="Submit" width="100" height="50"/>
+                    </form> */}
+
+                    {/* <input type="image" src="logg.png" name="saveForm" class="btTxt submit" id="saveForm" /> */}
+                    
+                    {/* <div style="position: absolute; left: 10px; top: 40px;"> 
+                        <img 
+                            src="logg.png" 
+                            width="114" 
+                            height="38" 
+                            onclick="DoSomething();"
+                        />
+                    </div> */}
+
                     <div className="d-flex plant-types">
-                        <div className="justify-content-center">
-                            <img className="plants btn img-fluid" src={usual}/>
+                        <form action="/submit-url" method="post" className="justify-content-center">
+                            <input type="image"  alt="Submit" className="plants img-fluid" src={usual}/>
                             <p className="text-white"><strong>Standard House Plants</strong></p>
-                        </div>
-                        <div className="justify-content-center">
-                            <img className="plants btn img-fluid" src={succulents}/>
+                        </form>
+                        <form action="/submit-url" method="post" className="justify-content-center">
+                            <input type="image"  alt="Submit" className="plants img-fluid" src={succulents}/>
                             <p className="text-white"><strong>Succulents</strong></p>
-                        </div>
-                        <div className="justify-content-center">
-                            <img className="plants btn img-fluid" src={orchids}/>
+                        </form>
+                        <form action="/submit-url" method="post" className="justify-content-center">
+                            <input type="image"  alt="Submit" className="plants img-fluid" src={orchids}/>
                             <p className="text-white"><strong>Orchids</strong></p>
-                        </div>
-                        <div className="justify-content-center">
-                            <img className="plants btn img-fluid" src={carnivorous}/>
+                        </form>
+                        <form action="/submit-url" method="post" className="justify-content-center">
+                            <input type="image"  alt="Submit" className="plants img-fluid" src={carnivorous}/>
                             <p className="text-white"><strong>Carnivorous</strong></p>
-                        </div>
-                        <div className="justify-content-center">
-                            <img className="plants btn img-fluid" src={unusual}/>
+                        </form>
+                        <form action="/submit-url" method="post" className="justify-content-center">
+                            <input type="image"  alt="Submit" className="plants img-fluid" src={unusual}/>
                             <p className="text-white"><strong>Unusual / Rare</strong></p>
-                        </div>
-                        <div className="justify-content-center">
-                            <img className="plants btn img-fluid" src={landscape}/>
+                        </form>
+                        <form action="/submit-url" method="post" className="justify-content-center">
+                            <input type="image"  alt="Submit" className="plants img-fluid" src={landscape}/>
                             <p className="text-white"><strong>Landscape</strong></p>
-                        </div>
-                        <div className="justify-content-center">
-                            <img className="plants btn img-fluid" src={outdoors}/>
+                        </form>
+                        <form action="/submit-url" method="post" className="justify-content-center">
+                            <input type="image"  alt="Submit" className="plants img-fluid" src={outdoors}/>
                             <p className="text-white"><strong>Outdoor Potted Plants</strong></p>
-                        </div>
-                        <div className="justify-content-center">
-                            <img className="plants btn img-fluid" src={veggies}/>
+                        </form>
+                        <form action="/submit-url" method="post" className="justify-content-center">
+                            <input type="image"  alt="Submit" className="plants img-fluid" src={veggies}/>
                             <p className="text-white"><strong>Vegetable Gardens</strong></p>
-                        </div>
+                        </form>
                     </div>
                 </div>
             </div>
