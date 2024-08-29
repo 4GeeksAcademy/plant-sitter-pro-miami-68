c596e3c55f3cae917b14149b130b58e215be466a
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-08-29 15:18:43 +0000

    updated how it works and blog pages

--- src/front/js/pages/howItWorks.js
+++ src/front/js/pages/howItWorks.js
@@ -16,55 +16,56 @@ export const HowItWorks = () => {
 	return (
 		<div className="text-center mt-5">
             <h1 className="diphylleia-regular mb-4"><strong>How it Works</strong></h1>
-            <h2 className="diphylleia-regular mb-5 mt-3"><strong>We connect people who need help taking care of their plants with people who have green thumbs</strong></h2>
-
+            <h2 className="diphylleia-regular col-9 m-auto mb-5 mt-5"><strong>We connect people who need help taking 
+                care of their plants with people who have green thumbs</strong>
+            </h2>
             <div className="row container-fluid">
                 <div className="col m-3">
-                    <h2>Need help with your plants? <img src={wilting}/></h2>
+                    <h3>Need help with your plants? <img src={wilting}/></h3>
                     <div className="container works text-start">
                         <button
                             type="submit" 
-                            className="btn col-6 rounded-pill"
+                            className="btn col-12 rounded-pill mt-4"
                             onClick={
                                 () => {
                                     navigate('/client-map')
                                 }
                             }
                         >
-                            <h1 className="diphylleia-regular text-center">
+                            <h3 className="diphylleia-regular text-center">
                                 Hire a Plant Sitter
-                            </h1>
+                            </h3>
                         </button>
-                        <h2 className="diphylleia-regular mt-3"><strong>🌿Step 1: Tell us where you're located.</strong></h2>
-                        <p className="mb-3 fs-4">We'll tell you how many plant sitters are available near you.</p>
-                        <h2 className="diphylleia-regular mt-3"><strong>🌿Step 2: Subscribe to our service and create an account.</strong></h2>
-                        <p className="mb-3 fs-4">Subscribing requires a low, one time fee that grants you access to our network of vetted, plant care professionals. You can cancel any time.</p>
-                        <h2 className="diphylleia-regular mt-3"><strong>🌿Step 3: Create a job post and secure the plant care services that you need.</strong></h2>
-                        <p className="mb-3 fs-4">Here you'll answer questions about the number and types of plants that you have, as well as the kinds of services that you require.</p>
+                        <h4 className="diphylleia-regular mt-4"><strong>🌿Step 1: Tell us where you're located.</strong></h4>
+                        <p className="mb-4 fs-5">We'll tell you how many plant sitters are available near you.</p>
+                        <h4 className="diphylleia-regular mt-3"><strong>🌿Step 2: Subscribe to our service and create an account.</strong></h4>
+                        <p className="mb-4 fs-5">Subscribing requires a low, one time fee that grants you access to our network of vetted, plant care professionals. You can cancel any time.</p>
+                        <h4 className="diphylleia-regular mt-3"><strong>🌿Step 3: Create a job post and secure the plant care services that you need.</strong></h4>
+                        <p className="mb-4 fs-5">Here you'll answer questions about the number and types of plants that you have, as well as the kinds of services that you require.</p>
                     </div>
                 </div>
                 <div className="col m-3">
-                    <h2><img src={helping}/> Looking for work in plant care?</h2>
+                    <h3><img src={helping}/> Looking for work in plant care?</h3>
                     <div className="container works text-start">
                     <button
                             type="submit" 
-                            className="btn col-6 rounded-pill"
+                            className="btn col-12 rounded-pill mt-4"
                             onClick={
                                 () => {
                                     navigate('/provider-map')
                                 }
                             }
                         >
-                            <h1 className="diphylleia-regular text-center">
+                            <h3 className="diphylleia-regular text-center">
                                 Become a Plant Sitter
-                            </h1>
+                            </h3>
                         </button>
-                        <h2 className="diphylleia-regular mt-3"><strong>🌿Step 1: Tell us where you're located and how far you're willing to travel.</strong></h2>
-                        <p className="mb-3 fs-4">We'll tell you how many jobs are available in your search area.</p>
-                        <h2 className="diphylleia-regular mt-3"><strong>🌿Step 2: Subscribe to our service and create an account.</strong></h2>
-                        <p className="mb-3 fs-4">Subscribing requires a low, one time fee that grants you access to view and accept jobs in your area. You can cancel any time.</p>
-                        <h2 className="diphylleia-regular mt-3"><strong>🌿Step 3: Create a profile and find work near you as a plant care professional.</strong></h2>
-                        <p className="mb-3 fs-4">Here you'll answer questions about your training and experience, and you'll have access to veiw and accept jobs near you.</p>
+                        <h4 className="diphylleia-regular mt-4"><strong>🌿Step 1: Tell us where you're located and how far you're willing to travel.</strong></h4>
+                        <p className="mb-4 fs-5">We'll tell you how many jobs are available in your search area.</p>
+                        <h4 className="diphylleia-regular mt-3"><strong>🌿Step 2: Subscribe to our service and create an account.</strong></h4>
+                        <p className="mb-4 fs-5">Subscribing requires a low, one time fee that grants you access to view and accept jobs in your area. You can cancel any time.</p>
+                        <h4 className="diphylleia-regular mt-3"><strong>🌿Step 3: Create a profile and find work near you as a plant care professional.</strong></h4>
+                        <p className="mb-4 fs-5">Here you'll answer questions about your training and experience, and you'll have access to veiw and accept jobs near you.</p>
                     </div>
                 </div>
             </div>
