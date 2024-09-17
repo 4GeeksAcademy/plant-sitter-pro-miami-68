972da37b9bcb13ad628ac02266a0b3f2eb8deeb1
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-09-17 21:51:02 +0000

    wip landing page updates

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -7,6 +7,9 @@ import { useNavigate } from "react-router-dom";
 import landing from "../../img/landing-page.jpg";
 import landing2 from "../../img/landing-page2.png";
 import logo2 from "../../img/Logo2.png";
+import wilting from "../../img/wilting.png";
+import helping from "../../img/helping.png";
+import border from "../../img/border.png";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -23,6 +26,60 @@ export const Home = () => {
 				<div class="hero-text">
 					<h1 className="diphylleia-regular lp-headers">We connect plant owners</h1>
 					<h1 className="diphylleia-regular lp-headers">with plant caretakers</h1>
+
+
+					<div className="text-center mt-5">
+						<div className="row container">
+							<div className="col-1"></div>
+							<div className="col-5 get-started">
+								<h1 className="diphylleia-regular">Plant Owners</h1>
+								<h4>🌿Going out of town?</h4>
+								<h4>🌿Need help with pests?</h4>
+								<h4>🌿Love plants, but kill them?</h4>
+								{/* <img src={wilting} /> */}
+								{/* <div className="fs-4">Start a free search for professionals near you.</div> */}
+								<div className="container text-start">
+									<button
+										type="submit"
+										className="btn col-12 rounded-pill mt-4"
+										onClick={
+											() => {
+												navigate('/client-services1')
+											}
+										}
+									>
+										<h3 className="diphylleia-regular text-center">
+											<strong>Find Help</strong>
+										</h3>
+									</button>
+								</div>
+							</div>
+							<div className="col-5 get-started">
+								<h1 className="diphylleia-regular">Caretakers</h1>
+								<h4>🌿Got a green thumb?</h4>
+								<h4>🌿Looking for work?</h4>
+								{/* <img src={wilting} /> */}
+								<div className="container text-start">
+									<button
+										type="submit"
+										className="btn col-12 rounded-pill mt-4"
+										onClick={
+											() => {
+												navigate('/provider-map')
+											}
+										}
+									>
+										<h3 className="diphylleia-regular text-center">
+											<strong>Find Jobs</strong>
+										</h3>
+									</button>
+								</div>
+							</div>
+							{/* <div className="col-2"></div> */}
+						</div>
+					</div>
+
+
 				</div>
 			</div>
 			{/* <img className="landing-page" src={landing}/> */}
