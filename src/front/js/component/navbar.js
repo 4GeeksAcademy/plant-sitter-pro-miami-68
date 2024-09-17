755a14387918aba45c61894d7477e61d1c45c135
Thisisgerry123 <167911356+Thisisgerry123@users.noreply.github.com>
2024-09-17 03:29:03 +0000

    added a cursor pointer to the Navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import { Link, useNavigate } from "react-router-dom";
 import logo2 from "../../img/Logo2.png";
+import "../../styles/navbar.css"
 
 export const Navbar = () => {
   const { store, actions } = useContext(Context);
