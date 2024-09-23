eaabfcbf6d94b89b6e2fe489ab50ab5472559a32
Thisisgerry123 <167911356+Thisisgerry123@users.noreply.github.com>
2024-09-23 00:37:08 +0000

    created the bushttrimming component to load in pages

--- src/front/js/component/BushTrimmingLoader.js
+++ src/front/js/component/BushTrimmingLoader.js
@@ -3,13 +3,15 @@ import '../../styles/BushTrimmingLoader.css';
 
 const BushTrimmingLoader = () => {
   return (
-    <div className="loading-container">
-      <div className="bush"></div>
-      <div className="scissors">
-        <div className="blade blade-left"></div>
-        <div className="blade blade-right"></div>
+    <div className="bush-trimming-loader-container">
+      <div className="loading-container">
+        <div className="bush"></div>
+        <div className="scissors">
+          <div className="blade blade-left"></div>
+          <div className="blade blade-right"></div>
+        </div>
+        <div className="hand"></div>
       </div>
-      <div className="hand"></div>
     </div>
   );
 };
