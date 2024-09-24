82f7cc3fb168c6c63a4077382e96d49104b65830
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-09-24 20:05:58 +0000

    migration file added

---
+++ migrations/versions/ce71cfc8b707_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: ce71cfc8b707
+Revises: ecacba484c68
+Create Date: 2024-09-24 19:45:54.196368
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ce71cfc8b707'
+down_revision = 'ecacba484c68'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
