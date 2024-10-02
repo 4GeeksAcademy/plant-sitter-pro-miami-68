9644104ac920c960a46dbac68aa50c7f28e1900c
Sedonia Raethstein <166647392+Sedonia-R@users.noreply.github.com>
2024-10-02 16:59:26 +0000

    updating main branch to align with develop

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 import PlantSitterCarousel from "../component/PlantSitterCard";
 import ServicesCarousel from "../component/ServicesCarousel";
